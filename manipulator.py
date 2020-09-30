b04a97561a50b0bb39562e94cecff7aec93bc423
Trisha Anand <trisha@appsmith.com>
2020-09-30 12:54:38 +0530

    Minor change in the input format of the excel sheet. Also now using UID as the unique indicator instead of village name (which may not be unique across different districts)

--- manipulator.py
+++ manipulator.py
@@ -40,15 +40,16 @@ def main(argv):
         currentSheet = theFile[sheet]
         
         for row in range(2, currentSheet.max_row + 1):
-            villageName = currentSheet.cell(row = row, column = 7).value
-            if villageName not in seen :
-                seen.add(villageName)
-                result[villageName] = []
-                villageDetails[villageName] = VillageDetail(currentSheet.cell(row = row, column = 3).value, 
-                currentSheet.cell(row = row, column = 4).value, 
-                currentSheet.cell(row = row, column = 5).value, 
-                currentSheet.cell(row = row, column = 6).value,
-                currentSheet.cell(row = row, column = 7).value)
+            uidVillage = currentSheet.cell(row = row, column = 3).value
+            if uidVillage not in seen :
+                seen.add(uidVillage)
+                result[uidVillage] = []
+                villageDetails[uidVillage] = VillageDetail(
+                    currentSheet.cell(row = row, column = 3).value, 
+                    currentSheet.cell(row = row, column = 4).value, 
+                    currentSheet.cell(row = row, column = 5).value, 
+                    currentSheet.cell(row = row, column = 6).value,
+                    currentSheet.cell(row = row, column = 7).value)
 
             # Add influencers
             Influencers = []
@@ -57,20 +58,29 @@ def main(argv):
             Influencers.append(Influencer(currentSheet.cell(row = row, column = 16).value, currentSheet.cell(row = row, column = 17).value, currentSheet.cell(row = row, column = 18).value, currentSheet.cell(row = row, column = 19).value))
             Influencers.append(Influencer(currentSheet.cell(row = row, column = 20).value, currentSheet.cell(row = row, column = 21).value, currentSheet.cell(row = row, column = 22).value, currentSheet.cell(row = row, column = 23).value))
             Influencers.append(Influencer(currentSheet.cell(row = row, column = 24).value, currentSheet.cell(row = row, column = 25).value, currentSheet.cell(row = row, column = 26).value, currentSheet.cell(row = row, column = 27).value))
+            Influencers.append(Influencer(currentSheet.cell(row = row, column = 28).value, currentSheet.cell(row = row, column = 29).value, currentSheet.cell(row = row, column = 30).value, currentSheet.cell(row = row, column = 31).value))
+            Influencers.append(Influencer(currentSheet.cell(row = row, column = 32).value, currentSheet.cell(row = row, column = 33).value, currentSheet.cell(row = row, column = 34).value, currentSheet.cell(row = row, column = 35).value))
+            Influencers.append(Influencer(currentSheet.cell(row = row, column = 36).value, currentSheet.cell(row = row, column = 37).value, currentSheet.cell(row = row, column = 38).value, currentSheet.cell(row = row, column = 39).value))
+            Influencers.append(Influencer(currentSheet.cell(row = row, column = 40).value, currentSheet.cell(row = row, column = 41).value, currentSheet.cell(row = row, column = 42).value, currentSheet.cell(row = row, column = 43).value))
+            Influencers.append(Influencer(currentSheet.cell(row = row, column = 44).value, currentSheet.cell(row = row, column = 45).value, currentSheet.cell(row = row, column = 46).value, currentSheet.cell(row = row, column = 47).value))
 
             Influencers = [x for x in Influencers if x.name]
             
-            result[villageName] += Influencers
+            result[uidVillage] += Influencers
             
             # Hotspot detail aggregation :
-            existing_details = hotspot.get(villageName)
+            existing_details = hotspot.get(uidVillage)
             if existing_details is None :
                 existing_details = HotspotDetails('', '', '', '')
 
-            sc = str(currentSheet.cell(row = row, column = 28).value or '')
-            people = (currentSheet.cell(row = row, column = 29).value or '')
-            village = str(currentSheet.cell(row = row, column = 30).value or '')
-            village_people = str(currentSheet.cell(row = row, column = 31).value or '')
+            try :
+                sc = str(currentSheet.cell(row = row, column = 48).value or '')
+            except :
+                print("Exception caught reading hotspot sc for village uid " + str(uidVillage) + ", where sc was " + currentSheet.cell(row = row, column = 48).value)
+
+            people = (currentSheet.cell(row = row, column = 49).value or '')
+            village = str(currentSheet.cell(row = row, column = 50).value or '')
+            village_people = str(currentSheet.cell(row = row, column = 51).value or '')
 
             new_sc = existing_details.sc
             new_people = existing_details.people
@@ -83,16 +93,13 @@ def main(argv):
                 try:
                     new_people = new_people + str(people).strip() + ","
                 except:
-                    print("Exception caught")
-                    print(villageName)
-                    print(people)
-                    # new_people = new_people + unicode(people, 'utf-8').strip() + ","
+                    print("Exception caught reading hotspot sc people for village uid " + str(uidVillage) + ", where people was " + people)
             
             if (village != '') :
                 new_village = new_village + village.strip() + ","
                 new_village_people = new_village_people + str(village_people).strip() + ","
         
-            hotspot[villageName] = HotspotDetails(new_sc, new_people, new_village, new_village_people)
+            hotspot[uidVillage] = HotspotDetails(new_sc, new_people, new_village, new_village_people)
             
     # Now lets start writing the new output excel sheet data
     header = ["UID", "District", "AC", "Block", "Village", "Hotspot SC", "Hotspot_SC_People", "Hotspot_Village", "Hotspot_Village_People", "Influencer Name", "Influencer Contact", "Influencer Occupation", "Influencer Party"]
